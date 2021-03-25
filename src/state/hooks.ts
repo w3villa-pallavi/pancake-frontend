3d8db3fce9d9195c50a46aa085694e111687525d
hachiojidev <hachiojidev@protonmail.com>
2021-03-25 16:06:58 +0900

    refactor: Add live data
    - Update types based on api
    - Renamed cards

--- src/state/hooks.ts
+++ src/state/hooks.ts
@@ -17,13 +17,12 @@ import {
   clear as clearToast,
   setBlock,
 } from './actions'
-import { State, Farm, Pool, Block, ProfileState, TeamsState, AchievementState, PriceState, RoundData } from './types'
+import { State, Farm, Pool, Block, ProfileState, TeamsState, AchievementState, PriceState } from './types'
 import { fetchProfile } from './profile'
 import { fetchTeam, fetchTeams } from './teams'
 import { fetchAchievements } from './achievements'
 import { fetchPrices } from './prices'
 import { initializePredictions } from './predictions'
-import { transformRoundResponse } from './predictions/helpers'
 
 export const useFetchPublicData = () => {
   const dispatch = useAppDispatch()
@@ -241,30 +240,20 @@ export const useInitializePredictions = () => {
 }
 
 export const useGetRounds = () => {
-  const rounds = useSelector((state: State) => state.predictions.rounds)
-
-  return useMemo<RoundData>(() => {
-    return Object.keys(rounds).reduce((accum, epoch) => {
-      return {
-        ...accum,
-        [epoch]: transformRoundResponse(rounds[epoch]),
-      }
-    }, {})
-  }, [rounds])
+  return useSelector((state: State) => state.predictions.rounds)
 }
 
 export const useGetCurrentEpoch = () => {
   return useSelector((state: State) => state.predictions.currentEpoch)
 }
 
-export const useIsNextRound = (epoch: number) => {
-  const currentEpoch = useGetCurrentEpoch()
-  return epoch === currentEpoch + 1
+export const useGetIntervalBlocks = () => {
+  return useSelector((state: State) => state.predictions.intervalBlocks)
 }
 
 export const useGetLiveRound = () => {
   const { currentEpoch, rounds } = useSelector((state: State) => state.predictions)
-  return rounds[currentEpoch]
+  return rounds.find((round) => round.epoch === currentEpoch)
 }
 
 export const useGetPredictionsStatus = () => {
@@ -273,6 +262,7 @@ export const useGetPredictionsStatus = () => {
 
 export const useGetCurrentRound = () => {
   const currentEpoch = useGetCurrentEpoch()
-  const roundData = useSelector((state: State) => state.predictions.rounds)
-  return roundData[currentEpoch]
+  const rounds = useSelector((state: State) => state.predictions.rounds)
+
+  return rounds.find((round) => round.epoch === currentEpoch)
 }
