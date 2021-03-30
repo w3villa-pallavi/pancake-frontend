feba0d22e531eb352a1aef30a2c0de2214332786
hachiojidev <hachiojidev@protonmail.com>
2021-03-30 16:31:42 +0900

    feat: Enter positions
    - Refactor polling query

--- src/state/types.ts
+++ src/state/types.ts
@@ -148,8 +148,8 @@ export interface BlockState {
 // Predictions
 
 export enum BetPosition {
-  BULL = 'bull',
-  BEAR = 'bear',
+  BULL = 'Bull',
+  BEAR = 'Bear',
 }
 
 export enum PredictionStatus {
@@ -172,6 +172,24 @@ export interface Round {
   bullBets: number
   bearAmount: number
   bullAmount: number
+  bets?: Bet[]
+}
+
+export interface Bet {
+  id: string
+  hash: string
+  amount: number
+  position: BetPosition
+  claimed: boolean
+  user: PredictionUser
+}
+
+export interface PredictionUser {
+  id: string
+  address: string
+  block: number
+  totalBets: number
+  totalBNB: number
 }
 
 export interface RoundData {
