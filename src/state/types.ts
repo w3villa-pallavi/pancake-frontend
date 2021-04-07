9cbc5340a7663a713afa0bd5402b2098b4838c26
hachiojidev <hachiojidev@protonmail.com>
2021-04-07 14:59:59 +0900

    feat: History pane filtering

--- src/state/types.ts
+++ src/state/types.ts
@@ -150,6 +150,7 @@ export interface BlockState {
 export enum BetPosition {
   BULL = 'Bull',
   BEAR = 'Bear',
+  HOUSE = 'House',
 }
 
 export enum PredictionStatus {
@@ -172,6 +173,7 @@ export interface Round {
   bullBets: number
   bearAmount: number
   bullAmount: number
+  position: BetPosition
   bets?: Bet[]
 }
 
@@ -197,19 +199,30 @@ export interface RoundData {
   [key: string]: Round
 }
 
+export interface BetData {
+  [key: string]: Bet[]
+}
+
+export enum HistoryFilter {
+  ALL = 'all',
+  COLLECTED = 'collected',
+  UNCOLLECTED = 'uncollected',
+}
+
 export interface PredictionsState {
   status: PredictionStatus
   isLoading: boolean
   isHistoryPaneOpen: boolean
   isChartPaneOpen: boolean
   isFetchingHistory: boolean
+  historyFilter: HistoryFilter
   currentEpoch: number
   currentRoundStartBlockNumber: number
   intervalBlocks: number
   bufferBlocks: number
   minBetAmount: string
   rounds: RoundData
-  bets: Bet[]
+  bets: BetData
 }
 
 // Global state
